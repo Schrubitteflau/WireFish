17aa9c95c4b689e6c25ea144c8312b18c7ae07f7
Schrubitteflau <45637360+Schrubitteflau@users.noreply.github.com>
2022-03-23 20:39:01 +0100

    Add link

--- sniff_modules/http/post_credentials.py
+++ sniff_modules/http/post_credentials.py
@@ -26,6 +26,7 @@ class Module(AbstractBaseModule):
             self.handle_http_response(packet=packet, response=packet[HTTPResponse])
 
     # https://github.com/invernizzi/scapy-http/blob/55976e4a5a7b73770f7fd68584c1237ccc15ef46/scapy_http/http.py#L117
+    # https://gist.github.com/cr0hn/cfa4e6d04a20f6248a506c072ae0ba81
     def guess_payload_class(self, payload):
         ''' Decides if the payload is an HTTP Request or Response, or something else '''
         try:
