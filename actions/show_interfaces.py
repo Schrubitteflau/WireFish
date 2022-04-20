38c7413c958ac02e5656117bbc56b5464d8a8eaf
Schrubitteflau <45637360+Schrubitteflau@users.noreply.github.com>
2022-04-20 19:43:28 +0200

    Fix actions/show_interfaces

--- actions/show_interfaces.py
+++ actions/show_interfaces.py
@@ -1,5 +1,10 @@
-from scapy.all import *
+#from scapy.all import conf, IFACES, show_interfaces
+from scapy.all import get_if_list
 
+# Crash depuis que 2.4.5 -> 2.4.4
 def show_interfaces() -> None:
-    print(conf.ifaces)
+    print(get_if_list())
+    #print(conf.ifaces)
+    #IFACES.show()
+    #show_interfaces()
 
