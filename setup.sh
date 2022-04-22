b39784e0736bc306926a52031063a5bb2fcf4cdc
Schrubitteflau <45637360+Schrubitteflau@users.noreply.github.com>
2022-04-22 21:06:31 +0200

    Improve setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,17 @@ source $VENV_NAME/bin/activate
 # Installation des dépendances
 pip install wheel
 pip install -r requirements.txt
+
 # Ajout du she-bang pointant vers le binaire du venv au début de main.py
-sed -i "1i#! $VENV_NAME/bin/python\n" main.py
+# Seulement s'il n'est pas déjà présent, car parfois on peut être amené à
+# réinstaller le venv et donc réexecuter le script
+first_line=$(head -1 main.py)
+she_bang="#! $VENV_NAME/bin/python"
+
+if [[ $first_line != $she_bang ]]; then
+    sed -i "1i$she_bang\n" main.py
+fi
+
 # On le rend exécutable
 chmod +x main.py
 
