d41ab367849065dcb3d0d183671a64f70973d8f2
Schrubitteflau <45637360+Schrubitteflau@users.noreply.github.com>
2022-04-24 20:54:33 +0200

    Improve passive mode management in ftp save_files module

--- sniff_modules/ftp/save_files.py
+++ sniff_modules/ftp/save_files.py
@@ -1,23 +1,39 @@
-from scapy.all import Packet, Raw, TCP, IP
+from typing import Optional, TypedDict
+from scapy.all import Packet, Raw, TCP
 
 from sniff_modules.AbstractBaseModule import AbstractBaseModule
-from util.packet_analyze.ftp import FTPPacketParser
+from util.packet_analyze.ftp import FTPPacketParser, FTPPassiveModeHandlerCollection
+
+class PassiveModeParams(TypedDict):
+    client_ip: str
+    client_port: int
 
 class Module(AbstractBaseModule):
 
     def __init__(self) -> None:
         super().__init__()
-        # Command PASV, code 227, reset if code 226
-        self.passive_mode = None
+        self._opened_passive_modes = FTPPassiveModeHandlerCollection()
 
     def module_name(self):
         return "ftp.save_files"
 
     def on_receive_packet(self, packet: Packet) -> None:
-        if packet.haslayer(TCP) and packet.haslayer(Raw):
-            ftp_packet_parser = FTPPacketParser(packet=packet)
+        passive_mode_handler = self._opened_passive_modes.get_handler_for_packet(packet=packet)
 
+        if passive_mode_handler is not None:
+            passive_mode_handler.handle_packet(packet=packet)
+
+        elif packet.haslayer(TCP) and packet.haslayer(Raw):
+            ftp_packet_parser = FTPPacketParser(packet=packet)
+            # Code "227" : response for command "PASV"
             if ftp_packet_parser.is_entering_passive_mode_response():
                 (ip, port) = ftp_packet_parser.get_entering_passive_mode_response_parameters()
-                print(ip, port)
+                self._opened_passive_modes.register_handler(client_ip=ip, client_port=port)
+                self._opened_passive_modes.print()
+            # Code "226", example : "226 Directory send OK." 
+            elif ftp_packet_parser.is_transfer_done_response():
+                self._opened_passive_modes.destroy_last_created_handler()
+            # Code "421 Timeout."
+            elif ftp_packet_parser.is_timeout_response():
+                self._opened_passive_modes.destroy_all_handlers()
 
--- util/packet_analyze/ftp.py
+++ util/packet_analyze/ftp.py
@@ -1,7 +1,7 @@
 import re
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Dict
 
-from scapy.all import Packet, Raw
+from scapy.all import Packet, Raw, IP, TCP, NoPayload
 from util.misc import add_space_and_encode_to_bytes
 
 passive_mode_response_regex = re.compile(
@@ -36,9 +36,17 @@ class FTPPacketParser:
         # We handle a response code as a command : a string which prefix the rest of the data separated by a space
         return self.is_command(response_code)
 
+    # See more about passive mode : https://www.deskshare.com/resources/articles/ftp-how-to.aspx
     def is_entering_passive_mode_response(self) -> bool:
         return self.is_response_code("227")
+
+    # https://fr.wikipedia.org/wiki/Liste_des_codes_des_r%C3%A9ponses_d%27un_serveur_FTP
+    def is_transfer_done_response(self) -> bool:
+        return self.is_response_code("226")
     
+    def is_timeout_response(self) -> bool:
+        return self.is_response_code("421")
+
     def get_entering_passive_mode_response_parameters(self) -> Tuple[str, int]:
         """
             You must ensure that is_entering_passive_mode_response() returns true before calling this method
@@ -57,7 +65,68 @@ class FTPPacketParser:
             port_octet_1 = int(result.group(5))
             port_octet_2 = int(result.group(6))
             port = port_octet_1 * 256 + port_octet_2
+            # It means that the server expects the client to send the data in the specified IP and PORT
             return (ip, port)
         # Erreur
         return None
+
+
+class FTPPassiveModeHandler:
+    def __init__(self, client_ip: str, client_port: int) -> None:
+        self._client_ip = client_ip
+        self._client_port = client_port
+
+    def handle_packet(self, packet: Packet) -> None:
+        # We assume that the packet has already been checked before and that it is destinated to this handler
+        payload = packet[TCP].payload
+        if not isinstance(payload, NoPayload):
+            print("{}:{} => handle payload size : {}".format(self._client_ip, self._client_port, len(payload)))
+
+
+class FTPPassiveModeHandlerCollection:
+    def __init__(self) -> None:
+        # Key : "IP:PORT", value : FTPPassiveModeHandler instance
+        self._passive_mode_handlers: Dict = {}
+        self._last_created_handler_key: Optional[str] = None
+
+    def _make_key(self, ip: str, port: int) -> str:
+        return "{}:{}".format(ip, port)
+
+    def _retrieve_handler(self, client_ip: str, client_port: int) -> Optional[FTPPassiveModeHandler]:
+        key = self._make_key(ip=client_ip, port=client_port)
+        if key in self._passive_mode_handlers:
+            return self._passive_mode_handlers[key]
+        return None
+
+    def register_handler(self, client_ip: str, client_port: int) -> None:
+        key = self._make_key(ip=client_ip, port=client_port)
+        handler = FTPPassiveModeHandler(client_ip=client_ip, client_port=client_port)
+        self._passive_mode_handlers[key] = handler
+        self._last_created_handler_key = key
+
+    def destroy_last_created_handler(self) -> None:
+        if self._last_created_handler_key is not None:
+            del self._passive_mode_handlers[self._last_created_handler_key]
+            self._last_created_handler_key = None
+
+    def destroy_all_handlers(self) -> None:
+        self._passive_mode_handlers.clear()
+    
+    def print(self) -> None:
+        print(self._passive_mode_handlers)
+
+    def get_handler_for_packet(self, packet: Packet) -> Optional[FTPPassiveModeHandler]:
+        if len(self._passive_mode_handlers) > 0 and packet.haslayer(IP) and packet.haslayer(TCP):
+            # Based on source IP and PORT
+            ip_src=packet[IP].src
+            tcp_sport=packet[TCP].sport
+            handler = self._retrieve_handler(client_ip=ip_src, client_port=tcp_sport)
+
+            # Based on destination IP and PORT
+            if handler is None:
+                ip_dst=packet[IP].dst
+                tcp_dport=packet[TCP].dport
+                handler = self._retrieve_handler(client_ip=ip_dst, client_port=tcp_dport)
+            
+            return handler
 
