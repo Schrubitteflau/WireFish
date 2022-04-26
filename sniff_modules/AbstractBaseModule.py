e65fb44beff4717badf93c0bc6da43259105f6f1
Schrubitteflau <45637360+Schrubitteflau@users.noreply.github.com>
2022-04-27 00:08:15 +0200

    Update AbstractBaseModule.write_file() : now writting in a module-specific directory and timestamp is included in the file name

--- sniff_modules/AbstractBaseModule.py
+++ sniff_modules/AbstractBaseModule.py
@@ -1,4 +1,5 @@
-from typing import Union
+import os
+from typing import Union, Tuple
 from abc import ABC, abstractmethod
 from datetime import datetime
 
@@ -30,11 +31,16 @@ class AbstractBaseModule(ABC):
     def log_error(self, error_message: str) -> None:
         self.log(error_message, "red")
 
-    def write_file(self, filename: str, content: Union[bytes, bytearray], append_flag: bool = False) -> int:
+    def write_file(self, filename: str, content: Union[bytes, bytearray], append_flag: bool = False) -> Tuple[str, int]:
         open_mode = "ab" if append_flag else "wb"
+        timestamp_now = int(datetime.timestamp(datetime.now()))
+        real_filename = "{}_{}".format(timestamp_now, filename)
+        file_path = "sniff_files/{}/{}".format(self.module_name(), real_filename)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
         try:
-            with open(filename, open_mode) as file:
-                return file.write(content)
+            with open(file_path, open_mode) as file:
+                written_size = file.write(content)
+                return (file_path, written_size)
         except Exception as e:
             self.log_error(e)
 
