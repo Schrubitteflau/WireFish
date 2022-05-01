f63e9a4c2afbcd067ba7b5e9b297f709e3ffe185
Schrubitteflau <45637360+Schrubitteflau@users.noreply.github.com>
2022-05-01 19:43:18 +0200

    Update setup.sh : add 'install' or 'reinstall' parameter

--- setup.sh
+++ setup.sh
@@ -1,10 +1,27 @@
+# Nom de dossier de l'environnement virtuel Python
 VENV_NAME="wirefish-venv"
 
-if [ -d "$VENV_NAME" ]; then
-    echo "venv '$VENV_NAME' déjà installé dans le dossier courant"
-    exit
+if [ $# -eq 0 ]; then
+    echo "Veuillez spécifier le paramètre 'reinstall' ou 'install'"
+    exit 1
 fi
 
+if [[ "$1" == "install" ]]; then
+    if [ -d "$VENV_NAME" ]; then
+        echo "venv '$VENV_NAME' déjà installé dans le dossier courant"
+        exit 1
+    fi
+elif [[ "$1" == "reinstall" ]]; then
+    if [ -d "$VENV_NAME" ]; then
+        echo "Suppression du dossier de venv $VENV_NAME"
+        rm -r $VENV_NAME
+    fi
+else
+    echo "Paramètre invalide '$1'"
+    exit 1
+fi
+
+# Création du venv
 python3 -m venv $VENV_NAME
 # Activation du venv (met à jour et ajoute des variables d'environnement)
 source $VENV_NAME/bin/activate
@@ -18,11 +35,11 @@ pip install -r requirements.txt
 first_line=$(head -1 main.py)
 she_bang="#! $VENV_NAME/bin/python"
 
-if [[ $first_line != $she_bang ]]; then
+if [[ "$first_line" != "$she_bang" ]]; then
     sed -i "1i$she_bang\n" main.py
 fi
 
 # On le rend exécutable
 chmod +x main.py
 
-echo "Vous pouvez entrer './main.py' pour démarrer Wirefish :)"
+printf "\nVous pouvez entrer './main.py' pour démarrer Wirefish :)\n"
